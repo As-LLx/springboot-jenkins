706dee3aea8ee048b5e052c33d2d479889a0dde9
as_llx <1064556353@qq.com>
2021-03-13 17:34:41 +0800

    init

---
+++ src/main/java/com/quanlvsan/HightTea/HightTeaApplication.java
@@ -0,0 +1,13 @@
+package com.quanlvsan.HightTea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HightTeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HightTeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quanlvsan/HightTea/HightTeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanlvsan.HightTea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HightTeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
