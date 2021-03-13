e99cd6457e4110882e2ca30481b357007eef9c59
as_llx <1064556353@qq.com>
2021-03-13 18:49:26 +0800

    test01

--- src/main/java/com/quanlvsan/HightTea/HightTeaApplication.java
+++ src/main/java/com/quanlvsan/HightTea/HightTeaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HightTeaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HightTeaApplication.class, args);
+		 SpringApplication.run(HightTeaApplication.class, args);
 	}
 
 }
